d3e06e20fc20c5de0d6924b828b5d8304867b781
Maris Orbidans <maris.orbidans@gmail.com>
2017-07-03 23:10:02 +0100

    change keymap

--- src/keymap_default.c
+++ src/keymap_default.c
@@ -21,8 +21,8 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * `-----------------------------------------------------------------'
      */
     KEYMAP(
-        ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, NO,  INS,\
-        TAB,    Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,   DEL, \
+        ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS, GRV,  INS,\
+        TAB,    Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,   DEL, \
         CAPS,    A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,\
         LSFT,  NO,  Z,   X,   C,   V,   B,   N,   M,   COMM, DOT, SLSH, RSFT,NO, UP,\
         LCTL,LGUI,LALT,NO,      SPC,                     RALT,RCTL, FN0, LEFT, DOWN, RGHT),
