101b03c6f102f9b7ffa2b83d8aa95c0b1c00dfdf
Maris Orbidans <maris.orbidans@gmail.com>
2017-07-08 07:10:37 +0100

    change keymap

--- src/keymap_default.c
+++ src/keymap_default.c
@@ -21,11 +21,11 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * `-----------------------------------------------------------------'
      */
     KEYMAP(
-        ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS, GRV,  INS,\
+        ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS, GRV,  HOME,\
         TAB,    Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,   DEL, \
-        CAPS,    A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,\
+        LCTL,    A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,\
         LSFT,  NO,  Z,   X,   C,   V,   B,   N,   M,   COMM, DOT, SLSH, RSFT,NO, UP,\
-        LCTL,LGUI,LALT,NO,      SPC,                     RALT,RCTL, FN0, LEFT, DOWN, RGHT),
+        RCTL,LALT, LGUI ,NO,      SPC,                     RGUI,FN0, RALT, LEFT, DOWN, RGHT),
     /* Keymap 1: FN Layer
      * ,----------------------------------------------------------.  ,---.
      * |  `| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Delete|  |INS|
@@ -40,10 +40,10 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * `-----------------------------------------------------------------'
      */
     KEYMAP(
-        GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL, TRNS, TRNS,\
-        TRNS,TRNS,UP,  TRNS,TRNS,TRNS,CALC,TRNS,INS, TRNS,PSCR,SLCK,PAUS,TRNS,      TRNS, \
-        TRNS,LEFT,DOWN,RGHT,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,HOME,PGUP,     TRNS, \
-        TRNS,NO,  TRNS,APP, FN1, FN2, FN3, VOLD,VOLU,MUTE,END, PGDN,TRNS,NO,  TRNS, \
+        PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, F13, DEL, PGUP,\
+        TRNS,TRNS,UP,  TRNS,TRNS,TRNS,CALC,TRNS,INS, TRNS,PSCR,SLCK,PAUS,CLEAR,      PGDN, \
+        TRNS,LEFT,DOWN,RGHT,TRNS,TRNS,MSTP,MNXT,MPRV,MPLY,HOME,PGUP,     TRNS, \
+        TRNS,NO,  TRNS,APP, TRNS, TRNS, TRNS, VOLD,VOLU,MUTE,END, PGDN,TRNS,NO,  TRNS, \
         TRNS,TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS)
 };
 
